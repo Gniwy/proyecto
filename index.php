18d9cd5418a159ba4b1ce23d771f897a5693c7a0
Cristian <cristian_castro112@hotmail.com>
2018-07-10 15:06:48 +0200

    recolocacion

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
   <!-- Bootstrap -->
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="css/header.css">
-  <link rel="stylesheet" href="css/footer.css">
+  <link rel="stylesheet" href="menu_top/css/header.css">
+  <link rel="stylesheet" href="footer/css/footer.css">
   <link rel="stylesheet" href="css/master.css">
 
 </head>
