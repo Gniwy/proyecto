9d4711bca0bf97b7a59562ce7afddc54cf52712c
Cristian <cristian_castro112@hotmail.com>
2018-07-11 17:33:01 +0200

    menu_top

--- modulos/menu_top/menu_top.php
+++ modulos/menu_top/menu_top.php
@@ -2,10 +2,12 @@
   <!-- Navbar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="col-8 col-sm-10 text-center navbar-brand" href="">Nombre del proyecto</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_sesion_login" aria-controls="navbar_sesion_login" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-    <div class="col-4 col-sm-2 collapse navbar-collapse" id="navbarSupportedContent">
+
+    <?php if(){ ?>
+    <div class="col-4 col-sm-2 collapse navbar-collapse" id="navbar_sesion_login">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -18,5 +20,7 @@
         </li>
       </ul>
     </div>
+  <?php } ?>
+
   </nav>
 </header>
