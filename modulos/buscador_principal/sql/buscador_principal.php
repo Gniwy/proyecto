fdcb7e9e38511af68aa26a9ba7194df6f0ea8b97
Toledo <josefueng93@hotmail.com>
2018-07-18 20:00:18 +0200

    bbdd actualiza por provincia y localidad y buscador acabandolo

---
+++ modulos/buscador_principal/sql/buscador_principal.php
@@ -0,0 +1,29 @@
+<?php
+require_once "../../../conexion/conexion.php";
+
+foreach($_GET as $variable => $valor){
+  $$variable=$valor;
+}
+
+// se crea un array donde se guarda las localidades
+$array_localidades= array('');
+
+$sql_localidad = 'SELECT nombre FROM localidad WHERE 1';
+$aux_provincia=mysqli_query($link, $sql_localidad);
+
+?>
+<select name="selecion_localidad" id="selecion_localidad">
+  <?php
+    while($ex_provincia=mysqli_fetch_assoc($aux_provincia)) {
+
+      // inserto cada valor devuelta en el $array_localidades
+      array_push($array_localidades, $ex_provincia);
+      // echo $ex_provincia['nombre'];
+      // print_r($array_localidades);
+      ?>
+
+
+
+    <?php } ?>
+
+</select>
--- proyecto.sql
+++ proyecto.sql
@@ -130,20 +130,94 @@ INSERT INTO `empresa` (`id`, `nombre`, `calle`, `cp`, `id_categoria`) VALUES
 --
 -- Estructura de tabla para la tabla `localidad`
 --
-
 CREATE TABLE `localidad` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `provincia_id` int(6) NOT NULL,
-  `cp` int(5) NOT NULL
+  `nombre` varchar(70) NOT NULL,
+  `provincia_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 --
 -- Volcado de datos para la tabla `localidad`
 --
 
-INSERT INTO `localidad` (`id`, `nombre`, `provincia_id`, `cp`) VALUES
-(1, 'fuengirola', 29, 29640);
+INSERT INTO `localidad` (`id`, `nombre`, `provincia_id`) VALUES
+(1, 'Ámsterdam', 1),
+(2, 'Haarlem', 1),
+(3, 'Zaanstad', 1),
+(4, 'Haarlemmermeer', 1),
+(5, 'Alkmaar', 1),
+(6, 'Amstelveen', 1),
+(7, 'Hilversum', 1),
+(8, 'Nissewaard', 1),
+(9, 'Purmerend', 1),
+(10, 'Hoorn', 1),
+(11, 'Velsen', 1),
+(12, 'Gooise Meren', 1),
+(13, 'Den Helder', 1),
+(14, 'Heerhugowaard', 1),
+(15, 'Róterdam', 2),
+(16, 'La Haya', 2),
+(17, 'Rijswijk', 2),
+(18, 'Zoetermeer', 2),
+(19, 'Leiden', 2),
+(20, 'Dordrecht', 2),
+(21, 'Alphen aan den Rijn', 2),
+(22, 'Westland', 2),
+(23, 'Delft', 2),
+(24, 'Schiedam', 2),
+(25, 'Spijkenisse', 2),
+(26, 'Leidschendam-Voorburg', 2),
+(27, 'Vlaardingen', 2),
+(28, 'Gouda', 2),
+(29, 'Capelle aan den IJssel', 2),
+(30, 'Katwijk', 2),
+(31, 'Lansingerland', 2),
+(32, 'Krimpenerwaard', 2),
+(33, 'Pijnacker-Nootdorp', 2),
+(34, 'Utrecht', 3),
+(35, 'Amersfoort', 3),
+(36, 'Veenendaal', 3),
+(37, 'Zeist', 3),
+(38, 'Nieuwegein', 3),
+(39, 'Eindhoven', 4),
+(40, 'Tilburgo', 4),
+(41, 'Breda', 4),
+(42, 'Bolduque', 4),
+(43, 'Helmond', 4),
+(44, 'Oss', 4),
+(45, 'Roosendaal', 4),
+(46, 'Bergen op Zoom', 4),
+(47, 'Oosterhout', 4),
+(48, 'Groningen', 5),
+(49, 'Almere', 6),
+(50, 'Lelystad', 6),
+(51, 'Nimega', 7),
+(52, 'Apeldoorn', 7),
+(53, 'Arnhem', 7),
+(54, 'Ede', 7),
+(55, 'Doetinchem', 7),
+(56, 'Barneveld', 7),
+(57, 'Enschede', 8),
+(58, 'Zwolle', 8),
+(59, 'Deventer', 8),
+(60, 'Hengelo', 8),
+(61, 'Almelo', 8),
+(62, 'Hardenberg', 8),
+(63, 'Kampen', 8),
+(64, 'Emmen', 9),
+(65, 'Assen', 9),
+(66, 'Hoogeveen', 9),
+(67, 'Terneuzen', 10),
+(68, 'Leeuwarden', 11),
+(69, 'Smallingerland', 11),
+(70, 'De Fryske Marren', 11),
+(71, 'Heerenveen', 11),
+(72, 'Maastricht', 12),
+(73, 'Venlo', 12),
+(74, 'Sittard-Geleen', 12),
+(75, 'Heerlen', 12),
+(76, 'Roermond', 12);
 
 -- --------------------------------------------------------
 
@@ -151,75 +225,31 @@ INSERT INTO `localidad` (`id`, `nombre`, `provincia_id`, `cp`) VALUES
 -- Estructura de tabla para la tabla `provincias`
 --
 
-CREATE TABLE `provincias` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `slug` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `provincia` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
-  `comunidad_id` int(10) UNSIGNED NOT NULL,
-  `capital_id` int(11) NOT NULL DEFAULT '-1'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 CHECKSUM=1 COLLATE=utf8_spanish_ci DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
+CREATE TABLE `provincia` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `provincias`
+-- Volcado de datos para la tabla `provincia`
 --
 
-INSERT INTO `provincias` (`id`, `slug`, `provincia`, `comunidad_id`, `capital_id`) VALUES
-(1, 'alava', 'Álava', 16, 46),
-(2, 'albacete', 'Albacete', 8, 54),
-(3, 'alicante', 'Alicante', 10, 152),
-(4, 'almeria', 'Almería', 1, 292),
-(5, 'vila', 'Ávila', 7, 395),
-(6, 'badajoz', 'Badajoz', 11, 644),
-(7, 'illes-balears', 'Illes Balears', 4, 836),
-(8, 'barcelona', 'Barcelona', 9, 881),
-(9, 'burgos', 'Burgos', 7, 1220),
-(10, 'caceres', 'Cáceres', 11, 1580),
-(11, 'cadiz', 'Cádiz', 1, 1776),
-(12, 'castellon', 'Castellón', 10, 1844),
-(13, 'ciudad-real', 'Ciudad Real', 8, 1978),
-(14, 'cordoba', 'Córdoba', 1, 2065),
-(15, 'a-coruna', 'A Coruña', 12, 2150),
-(16, 'cuenca', 'Cuenca', 8, 2285),
-(17, 'girona', 'Girona', 9, 2526),
-(18, 'granada', 'Granada', 1, 2747),
-(19, 'guadalajara', 'Guadalajara', 8, 2947),
-(20, 'guipuzcoa', 'Guipúzcoa', 16, 3159),
-(21, 'huelva', 'Huelva', 1, 3257),
-(22, 'huesca', 'Huesca', 2, 3396),
-(23, 'jaen', 'Jaén', 1, 3545),
-(24, 'leon', 'León', 7, 3676),
-(25, 'lleida', 'Lleida', 9, 3918),
-(26, 'la-rioja', 'La Rioja', 17, 4124),
-(27, 'lugo', 'Lugo', 12, 4238),
-(28, 'madrid', 'Madrid', 13, 4356),
-(29, 'malaga', 'Málaga', 1, 4523),
-(30, 'murcia', 'Murcia', 14, 4588),
-(31, 'navarra', 'Navarra', 15, 4815),
-(32, 'ourense', 'Ourense', 12, 4925),
-(33, 'asturias', 'Asturias', 3, 5009),
-(34, 'palencia', 'Palencia', 7, 5137),
-(35, 'las-palmas', 'Las Palmas', 5, 5252),
-(36, 'pontevedra', 'Pontevedra', 12, 5312),
-(37, 'salamanca', 'Salamanca', 7, 5588),
-(38, 'santa-cruz-de-tenerife', 'Santa Cruz de Tenerife', 5, 5732),
-(39, 'cantabria', 'Cantabria', 6, 5823),
-(40, 'segovia', 'Segovia', 7, 6024),
-(41, 'sevilla', 'Sevilla', 1, 6152),
-(42, 'soria', 'Soria', 7, 6307),
-(43, 'tarragona', 'Tarragona', 9, 6499),
-(44, 'teruel', 'Teruel', 2, 6721),
-(45, 'toledo', 'Toledo', 8, 6934),
-(46, 'valencia', 'Valencia', 10, 7219),
-(47, 'valladolid', 'Valladolid', 7, 7415),
-(48, 'vizcaya', 'Vizcaya', 16, 7489),
-(49, 'zamora', 'Zamora', 7, 7821),
-(50, 'zaragoza', 'Zaragoza', 2, 8113),
-(51, 'ceuta', 'Ceuta', 18, 8115),
-(52, 'melilla', 'Melilla', 19, 8116);
-
+INSERT INTO `provincia` (`id`, `nombre`) VALUES
+(4, 'Brabante Septentrional'),
+(9, 'Drente'),
+(6, 'Flevoland'),
+(11, 'Frisia'),
+(5, 'Groninga'),
+(7, 'Güeldres'),
+(2, 'Holanda Meridional'),
+(1, 'Holanda Septentrional'),
+(12, 'Limburgo'),
+(8, 'Overijssel'),
+(3, 'Utrecht'),
+(10, 'Zelanda');
 -- --------------------------------------------------------
 
--- 
+--
 -- Estructura de tabla para la tabla `tipo_usuario`
 --
 
@@ -278,20 +308,19 @@ ALTER TABLE `empresa`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id_empresa` (`id`);
 
---
--- Indices de la tabla `localidad`
---
-ALTER TABLE `localidad`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `cp` (`cp`);
+  --
+  -- Indices de la tabla `localidad`
+  --
+  ALTER TABLE `localidad`
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `provincia_id` (`provincia_id`);
 
---
--- Indices de la tabla `provincias`
---
-ALTER TABLE `provincias`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `IDX_provincia` (`provincia`),
-  ADD KEY `FK_provincias` (`comunidad_id`);
+    --
+    -- Indices de la tabla `provincia`
+    --
+    ALTER TABLE `provincia`
+      ADD PRIMARY KEY (`id`),
+      ADD UNIQUE KEY `nombre` (`nombre`);
 
 --
 -- Indices de la tabla `tipo_usuario`
@@ -334,17 +363,17 @@ ALTER TABLE `comentario`
 ALTER TABLE `empresa`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT de la tabla `localidad`
---
-ALTER TABLE `localidad`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  --
+  -- AUTO_INCREMENT de la tabla `localidad`
+  --
+  ALTER TABLE `localidad`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
---
--- AUTO_INCREMENT de la tabla `provincias`
---
-ALTER TABLE `provincias`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+    --
+    -- AUTO_INCREMENT de la tabla `provincia`
+    --
+    ALTER TABLE `provincia`
+      MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `tipo_usuario`
@@ -356,6 +385,13 @@ ALTER TABLE `tipo_usuario`
 -- Restricciones para tablas volcadas
 --
 
+
+--
+-- Filtros para la tabla `localidad`
+--
+ALTER TABLE `localidad`
+  ADD CONSTRAINT `localidad_ibfk_1` FOREIGN KEY (`provincia_id`) REFERENCES `provincia` (`id`);
+COMMIT;
 --
 -- Filtros para la tabla `usuario`
 --
