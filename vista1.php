bb888c910e352a8c99008e9611f8135b4f01051e
Giwy <josefueng93@hotmail.com>
2018-09-03 22:44:11 +0200

    mapa buscador y coordenadas

--- vista1.php
+++ vista1.php
@@ -1,3 +1,8 @@
+<?php
+
+include ('conexion/conexion.php');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,128 +10,52 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Proyecto</title>
-
   <!-- Bootstrap -->
-  <link rel="stylesheet" href="style/bootstrap/css/bootstrap.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="style/css/header.css">
-  <link rel="stylesheet" href="style/css/footer.css">
-  <link rel="stylesheet" href="style/css/master.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/jquery-ui.css">
+  <link rel="stylesheet" href="modulos/menu_top/css/menu-top.css">
+  <link rel="stylesheet" href="modulos/footer/css/footer.css">
+  <!-- hoja de estilos de las paginas -->
+  <link rel="stylesheet" href="modulos/buscador_principal/css/buscador_principal.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="modulos/modulo_mapa/css/style.css">
 
+  <link rel="stylesheet" href="fontawesome/css/all.css">
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" />
+
+  <!-- mapa -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@0.7.7/dist/leaflet.css" />
+
+  <link rel="stylesheet" href="maps/docs/dist/style.css" />
+  <link rel="stylesheet" href="https://unpkg.com/leaflet-geosearch@latest/assets/css/leaflet.css" />
 </head>
 
 <body>
-  <!-- HEADER -->
-  <header>
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="col-8 col-sm-10 text-center navbar-brand" href="">Nombre del proyecto</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      <div class="col-4 col-sm-2 collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Mi cuenta
-              </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Login</a>
-              <a class="dropdown-item" href="#">Registrate</a>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
 
-  <!-- Contenido principal -->
-  <section class="container-fluid">
-    <div class="buscardorV1">
-      <div class="row">
-       <div class="col-6">
-        <h4 class="text-right">Buscar por: </h4>           
-       </div>
-        <div class="col-6 mt-2">        
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-              <label class="form-check-label" for="inlineRadio1">Localizacion</label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-              <label class="form-check-label" for="inlineRadio2">Trabajo</label>
-            </div>
-        </div>
-      </div>
-      <!--buscador segun seleccion en nueva busqueda(arreglar grid)-->
-       <div class="row mt-3 ml-5">           
-        <div class="row form-group col-9 mx-sm-3 mb-2">
-          <input type="text" class="col-6 col-sm-6 col-md-6 form-control" id="localidad" placeholder="Ej: Malaga">
-          <input type="text" class="col-6 col-sm-6 col-md-6 form-control" id="trabajo" placeholder="Ej: Carrefour">
-        </div>
-        <button type="submit" class="btn btn-primary mb-2">Buscar</button>
-      </div>
 
-    </div>
-  </section>
-  
-<!--FOOTER-->
-  <footer class="footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-5">
-          <h5><i class="fa fa-road"></i> Proyecto</h5>
-          <div class="row">
-            <div class="col-6">
-              <ul class="list-unstyled">
-                <li><a href="">Product</a></li>
-                <li><a href="">Benefits</a></li>
-                <li><a href="">Partners</a></li>
-                <li><a href="">Team</a></li>
-              </ul>
-            </div>
-            <div class="col-6">
-              <ul class="list-unstyled">
-                <li><a href="">Documentation</a></li>
-                <li><a href="">Support</a></li>
-                <li><a href="">Legal Terms</a></li>
-                <li><a href="">About</a></li>
-              </ul>
-            </div>
-          </div>
-          <ul class="nav">
-            <li class="nav-item"><a href="" class="nav-link pl-0"><i class="fa fa-facebook fa-lg"></i></a></li>
-            <li class="nav-item"><a href="" class="nav-link"><i class="fa fa-twitter fa-lg"></i></a></li>
-            <li class="nav-item"><a href="" class="nav-link"><i class="fa fa-github fa-lg"></i></a></li>
-            <li class="nav-item"><a href="" class="nav-link"><i class="fa fa-instagram fa-lg"></i></a></li>
-          </ul>
-          <br>
-        </div>
-        <div class="col-md-2">
-          <h5 class="text-md-right">Contactanos</h5>
-          <hr>
-        </div>
-        <div class="col-md-5">
-          <form>
-            <fieldset class="form-group">
-              <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Correo">
-            </fieldset>
-            <fieldset class="form-group">
-              <textarea class="form-control" id="exampleMessage" placeholder="mensaje"></textarea>
-            </fieldset>
-            <fieldset class="form-group text-xs-right">
-              <button type="button" class="btn btn-secondary-outline btn-lg">Enviar</button>
-            </fieldset>
-          </form>
-        </div>
-      </div>
-    </div>
-  </footer>
+<div id="app">
+</div>
 
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="style/jquery-3.3.1.js"></script>
+  <script src="js/jquery.js"></script>
+
+  <!-- jQuery 12.1.1 necessary jqueryUI -->
+  <script src="js/jquery-ui.js"></script>
+
   <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <script src="style/bootstrap/js/bootstrap.js"></script>
+  <script src="js/bootstrap.js"></script>
+
+  <script type="text/javascript" src="js/index.js"></script>
+
+  <script type="text/javascript" src="modulos/menu_top/js/menu_top.js"></script>
+
+  <!-- jQuery autocomplete (accion select_lugar) -->
+  <script type="text/javascript" src="modulos/buscador_principal/js/buscador_principal.js"></script>
+
+  <!-- mapa  -->
+  <script type="text/javascript" src="maps/docs/dist/bundle.min.js" charset="utf-8"></script>
+
 </body>
 
 </html>
