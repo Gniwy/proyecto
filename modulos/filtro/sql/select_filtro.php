cc6b6439227198d3a1e3c7092ebd84ae39c4e1a4
Toledo <josefueng93@hotmail.com>
2019-03-05 18:52:30 +0100

    .

--- modulos/filtro/sql/select_filtro.php
+++ modulos/filtro/sql/select_filtro.php
@@ -1,7 +1,14 @@
-<?php @include_once('../../../conexion/conexion.php'); ?>
-
 <?php
-if (!isset($_POST['c1'])) {
+
+@include_once('../../../conexion/conexion.php');
+
+foreach($_GET as $variable => $valor){
+  $$variable=$valor;
+}
+
+echo "-filtro1:".$filtro1."-filtro2:".$filtro2."-filtro3:".$filtro3."-filtro4:".$filtro4."---> Tole manco?";
+
+if (!isset($_GET['c1'])) {
   echo "string";
 }
  ?>
