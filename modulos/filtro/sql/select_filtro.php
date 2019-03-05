b68e86007a7af547bdba956fcaac768af7fa6c78
Cristian <cristian_castro112@hotmail.com>
2019-03-05 18:51:53 +0100

    .

--- modulos/filtro/sql/select_filtro.php
+++ modulos/filtro/sql/select_filtro.php
@@ -2,6 +2,12 @@
 
 @include_once('../../../conexion/conexion.php');
 
+$filtro1=null;
+$filtro2=null;
+$filtro3=null;
+$filtro4=null;
+
+
 foreach($_GET as $variable => $valor){
   $$variable=$valor;
 }
