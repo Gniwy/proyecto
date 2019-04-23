3363ed05e43e1ed86a87e46193a4e4ed83d66ba3
toledo <josefueng93@hotmail.com>
2019-04-23 13:38:09 +0200

    login css

--- modales/modal_login.php
+++ modales/modal_login.php
@@ -5,31 +5,29 @@
     <div class="modal-content">
       <div class="modal-header">
         <h4 class="modal-title col-md-9 text-left" id="modal_login_label">Login</h4>
-        <div class="col-md-3 text-right"><button type="button" class="close" data-dismiss="modal">&times;</button></div>
       </div>
       <div class="modal-body">
+        <input type="text" id="nick_email_login" placeholder="Username">
+        <input type="password" id="password_login"  placeholder="Password">
+        <p id="error_login"  style="color:red;">Email / contraseña incorrecta.<p>
+                <!-- <table>
+                  <tr>
+                    <th>Nick o email: </th>
+                    <th><input type="text" name="" placeholder="Nickname" id="nick_email_login" value=""></th>
+                  </tr>
+                  <tr>
+                    <th>Contraseña: </td>
+                    <td><input type="password" name="" placeholder="****" id="password_login" value=""></td>
+                  </tr>
 
-        <table>
-          <tr>
-            <th>Nick o email: </th>
-            <th><input type="text" name="" placeholder="Nickname" id="nick_email_login" value=""></th>
-          </tr>
-          <tr>
-            <th>Contraseña: </td>
-            <td><input type="password" name="" placeholder="****" id="password_login" value=""></td>
-          </tr>
-          <tr>
-            <th></td>
-            <td> <button type="button" class="btn btn-primary" name="button" id="btn_iniciar_sesion">Iniciar sesion</button> </td>
-          </tr>
-          <tr id="error_login">
-            <td></td>
-            <td style="color:red;">Email / contraseña incorrecta.</td>
-          </tr>
-        </table>
+                  <tr id="error_login">
+                    <td></td>
+                    <td style="color:red;">Email / contraseña incorrecta.</td>
+                  </tr>
+                </table> -->
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" name="button" id="btn_iniciar_sesion">Iniciar sesion</button>
       </div>
     </div>
 
