5059ff38a694694d715004a7df02c96df790eace
toledo <josefueng93@hotmail.com>
2019-04-23 13:54:44 +0200

    registro css

--- modales/modal_registro.php
+++ modales/modal_registro.php
@@ -1,15 +1,24 @@
 <div id="modal_registro" class="modal fade" role="dialog" aria-labelledby="modal_registro_label" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog registro" role="document">
 
     <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header">
         <h4 class="modal-title col-md-9 text-left" id="modal_registro_label">Registro</h4>
-        <div class="col-md-3 text-right"><button type="button" class="close" data-dismiss="modal">&times;</button></div>
+        
       </div>
       <div class="modal-body">
+        <input type="text" placeholder="Nickname" id="nick" name="" value="">
+                  <p id="nick_usado" style="color:red;">Nick en uso</p>
+        <input type="text" placeholder="Email" id="email" name="" value="">
+                  <p id="email_usado" style="color:red;">Email en uso</p>
+        <input type="password" placeholder="Contraseña" id="password" name="" value="">
+                  <p id="password_error_longitud" style="color:red;">Las contraseña tiene que tener como minimo 6 caracteres.</p>
+        <input type="password" name="" placeholder="Repetir contraseña" id="password_2" name="" value="">
+                  <p id="password_error" style="color:red;">Las contraseñas no coinciden</p>
 
-        <table>
+
+        <!-- <table>
           <tr>
             <th>Nick: </th>
             <th><input type="text" name="" placeholder="Nickname" id="nick" value=""></th>
@@ -46,10 +55,10 @@
             <td></td>
             <td><button type="button" class="btn btn-primary" name="button" id="btn_registro">Registrarme</button> <button type="button" id="btn_limpiar_campos_registro" class="btn btn-warning" name="button">Limpiar</button> </td>
           </tr>
-        </table>
+        </table> -->
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" name="button" id="btn_registro">Registrarme</button>
       </div>
     </div>
 
