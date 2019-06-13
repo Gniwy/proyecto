8cfd79e3dceb839dfb5ebfc135f270a1a82b0f56
toledo <josefueng93@hotmail.com>
2019-06-13 13:11:58 +0200

    poco style top and bot

--- modulos/menu_top/menu_top.php
+++ modulos/menu_top/menu_top.php
@@ -7,14 +7,15 @@ session_start();
 <header>
   <!-- Navbar -->
   <nav class="navbar navbar-expand-lg navbar-light">
-    <a class="text-left navbar-brand" id="titulo" href="http://ireferences.es"><img src="image/logo_ireference.png" class="" width=200 alt="logo"></a>
+    <a class="text-left navbar-brand d-none d-md-block" id="titulo" href="http://ireferences.es"><img src="image/logo_ireference.png" width=200 alt="logo"></a>
+    <a class="text-left navbar-brand d-block d-md-none" id="titulomobile" href="http://ireferences.es"><img src="image/logo_top.png"width=50 alt="logo"></a>
     <button class="col-1 col-sm-1 offset-10 navbar-toggler" id="botonDropdown" type="button" data-toggle="collapse" data-target="#navbar_sesion_login" aria-controls="navbar_sesion_login" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <?php if(!isset($_SESSION['id_usuario'])){ ?>
     <div class="collapse navbar-collapse contenidoPerfil" id="navbar_sesion_login">
       <ul class="navbar-nav que">
-        <li class="nav-item d-none d-md-block quEs">
+        <li class="nav-item quEs">
           <a class="nav-link" href="modulos/informacionWeb/queEs.php" id="ir" target="_blank">Que es IR</a>
         </li>
       </ul>
