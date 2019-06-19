404d82cac042b1c439d205a1d16b3a822ab0bb3c
Cristian <cristian_castro112@hotmail.com>
2019-06-19 20:27:18 +0200

    .

--- modulos/footer/js/footer.js
+++ modulos/footer/js/footer.js
@@ -1,18 +1,16 @@
-console.log('asdf');
+
 $('#btn_enviar_correo').click(function(){
-  alert('asdf');
+
   $.ajax({
     type:'POST',
     url:'modulos/footer/php/enviar_correo.php',
     data:{
       asdf:'asdf'
     }, success: function(data){
-      alert(data);
-        // window.location.href = "../crear_empresa/sql/datosCrearEmpresa.php";
+
+        window.location.href = "../crear_empresa/sql/datosCrearEmpresa.php";
 
     }
   });
-
-});
 
 });
